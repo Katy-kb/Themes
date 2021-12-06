d681f68c7a22171567de91a0845b381fac5be8c3
unknown <katy-kb@yandex.ru>
2021-12-06 11:49:12 +0300

    Themes

---
+++ app/src/main/java/katy/kb/a111/SecondActivity.kt
@@ -0,0 +1,26 @@
+package katy.kb.a111
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.content.Intent
+
+
+private const val HELLO_KEY = "hello"
+
+class SecondActivity : AppCompatActivity() {
+
+    lateinit var helloTextView: TextView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
+
+    helloTextView=findViewById(R.id.hello_text_view)
+
+   val helloValue:String?= intent.extras?.getString(HELLO_KEY)
+   helloTextView.text = helloValue
+
+    }
+}
